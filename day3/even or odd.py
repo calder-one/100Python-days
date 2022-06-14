05be52174ab1e3defead01eee58bbb7782532f39
ecalde24 <54601379+ecalde24@users.noreply.github.com>
2022-06-14 11:00:27 -0400

    added first day3 file

---
+++ day3/even or odd.py
@@ -0,0 +1,8 @@
+number = int(input("Which number do you want to check? "))
+
+#Write your code below this line 👇
+
+if number % 2 == 0:233
+    print("You are an even steven")
+else:
+    print("You are too odd")
