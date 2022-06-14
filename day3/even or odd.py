1f512c4dc3953b4aa1a5338492b9e9ce7df173f2
ecalde24 <54601379+ecalde24@users.noreply.github.com>
2022-06-14 15:51:36 -0400

    clean up even or odd

--- day3/even or odd.py
+++ day3/even or odd.py
@@ -2,7 +2,7 @@ number = int(input("Which number do you want to check? "))
 
 #Write your code below this line 👇
 
-if number % 2 == 0:233
+if number % 2 == 0:
     print("You are an even steven")
 else:
     print("You are too odd")
