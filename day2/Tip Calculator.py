1afffdb144c37b7b47796699ed81c7de8439b773
ecalde24 <54601379+ecalde24@users.noreply.github.com>
2022-06-14 16:05:49 -0400

    edited total print statement

--- day2/Tip Calculator.py
+++ day2/Tip Calculator.py
@@ -11,4 +11,4 @@ people = int(input("How many people are splitting the bill? "))
 total_with_tip = bill + tip_amount * bill / 100 
 per_person = round(total_with_tip / people, 3)
 total_per = "{:.2f}".format(per_person)
-print(f"Each person owes: ${total_per}")
+print(f"Each person total: ${total_per}")
