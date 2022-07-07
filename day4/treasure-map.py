116e4fa87eaefb0d29c3bcc6df45d4c7bccb4b65
ecalde24 <54601379+ecalde24@users.noreply.github.com>
2022-07-07 19:59:59 -0400

    adding treasure map

---
+++ day4/treasure-map.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+row1 = ["1","2","3"]
+row2 = ["4","5","6"]
+row3 = ["7","8","9"]
+map= [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+col = int(position[0])
+row = int(position[1])
+
+mark_row = map[row - 1] 
+mark_row[col - 1] = "X" # Final Mark within row or list number
