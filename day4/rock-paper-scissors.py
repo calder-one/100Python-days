0746d3acd32909c581d7537cf89565367d91c3a0
Erick Calderon <calderone.nmn@gmail.com>
2022-07-08 14:01:48 -0400

    adding rock-paper-scissors with ascii art

--- day4/rock-paper-scissors.py
+++ day4/rock-paper-scissors.py
@@ -1,11 +1,10 @@
 user_choice = int(input("Welcome to the game. Select 0 for Rock, 1 for Paper, 2 for Scissors\n"))
 computer_choice = random.randint(0, 2)
-options_list = ["rocks", "paper", "scissors"]
 
 print(f"You chose: {user_choice}")
 print(f"Computer chose: {computer_choice}")
 
-if user_choice > 2:
+if user_choice > 2 or user_choice < 0:
     print("Invalid number: You lose!")
 elif user_choice == computer_choice:
     print("It's a draw")
